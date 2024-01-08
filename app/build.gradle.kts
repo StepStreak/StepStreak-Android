149172db8ed48b776dc6cd5dfffc5493ad38af07
Mohamad <mrahmoun93@gmail.com>
2024-01-08 01:41:00 +0100

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.stepstreak.dev"
         minSdk = 26
         targetSdk = 34
-        versionCode = 20240108
+        versionCode = 202401080
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
