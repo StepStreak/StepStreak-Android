e329195476fb216817dd051243fb8b4c1270f8ca
Mohamad <mrahmoun93@gmail.com>
2024-10-27 02:56:40 +0200

    relase 1.3.4: prepare for multi locale

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,15 +13,15 @@ android {
         applicationId = "com.stepstreak.dev"
         minSdk = 26
         targetSdk = 34
-        versionCode = 202405023
-        versionName = "1.3.3"
+        versionCode = 202410027
+        versionName = "1.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         debug {
-            buildConfigField("String", "BASE_URL", "\"http://192.168.0.89:3000/\"")
+            buildConfigField("String", "BASE_URL", "\"http://192.168.0.210:3000/\"")
         }
         release {
             isMinifyEnabled = false
@@ -53,28 +53,27 @@ dependencies {
     implementation("dev.hotwire:strada:1.0.0-beta3")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.browser:browser:1.8.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("com.github.bumptech.glide:glide:4.15.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
-    implementation("com.google.android.gms:play-services-safetynet:18.0.1")
+    implementation("com.google.android.gms:play-services-safetynet:18.1.0")
 
-    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
-    implementation("com.google.firebase:firebase-messaging:24.0.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-messaging:24.0.3")
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.android.gms:play-services-fitness:21.2.0")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
-    implementation("com.google.android.play:integrity:1.3.0")
+    implementation("com.google.android.play:integrity:1.4.0")
 }
--- app/src/main/java/com/stepstreak/dev/util/Constants.kt
+++ app/src/main/java/com/stepstreak/dev/util/Constants.kt
@@ -1,7 +1,11 @@
 package com.stepstreak.dev.util
 
+import java.util.Locale
 import com.stepstreak.dev.BuildConfig
 
-val BASE_URL = BuildConfig.BASE_URL
-val HOME_URL = BASE_URL
-val SIGN_IN_URL = "$BASE_URL/signin"
+const val BASE_URL = BuildConfig.BASE_URL
+
+val locale: String = Locale.getDefault().language
+val HOME_URL = "$BASE_URL?locale=$locale"
+
+const val SIGN_IN_URL = "$BASE_URL/signin"
